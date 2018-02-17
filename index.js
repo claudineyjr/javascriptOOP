2bb16315fd61f5907e9a164346a7f294e4ec036d
claudineyjr <claudiney.alves@fatec.sp.gov.br>
2018-02-17 19:14:25 -0200

    Example of an Animal to explain how OOP works in pure javscript

---
+++ index.js
@@ -0,0 +1,30 @@
+// In Object Oriented Programming we always name the class with the first letter capitalized.
+// The instances of the class can be named as you wish. I always use the Camel Case: Ex: theFluffyCat
+function Animal (type, name, sound, age) {
+    this.type = type;
+    this.name = name;
+    this.sound = sound;
+    this.age = age;
+
+    this.makeASound = () => {
+        console.log(`${name} says: ${sound}`);
+    }
+}
+
+// Here I create a new instance of the Animal object
+const pudim = new Animal('cat', 'Pudim', 'meauou', 3);
+
+// Here I create other instance of the Animal object, with other properties
+const misty = new Animal('dog', 'Misty', 'Au Au Au', 1);
+
+// Here you can see all the properties of variable pudim
+console.log(pudim);
+
+// Here you can see all the properties of variable pudim
+console.log(misty);
+
+// Here I execute the makeASound() function of the variable pudim
+pudim.makeASound();
+
+// Here I execute the makeASound() function of the variable misty
+misty.makeASound();
