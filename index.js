62d9721c955d3e8cc228c4cd86e4eb27b4c65b8c
claudineyjr <claudiney.alves@fatec.sp.gov.br>
2018-02-18 22:33:17 -0300

    Class implementation

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // In Object Oriented Programming we always name the class with the first letter capitalized.
 // The instances of the class can be named as you wish. I always use the Camel Case: Ex: theFluffyCat
-function Animal (type, name, sound, age) {
-    this.type = type;
-    this.name = name;
-    this.sound = sound;
-    this.age = age;
+class Animal {
+    constructor(type, name, sound, age) {
+        this.type = type;
+        this.name = name;
+        this.sound = sound;
+        this.age = age;
+    }
+    
 
-    this.makeASound = () => {
-        console.log(`${name} says: ${sound}`);
+    makeASound() {
+        console.log(`${this.name} says: ${this.sound}`);
     }
 }
 
